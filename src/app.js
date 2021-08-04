87f9733ee989bd6a2f151afa201956905dff5c35
Paulo Henrique <paulohgs@alu.ufc.br>
2021-08-04 16:20:11 -0300

    HOTFIX: Resolução de conflitos

--- src/app.js
+++ src/app.js
@@ -7,38 +7,40 @@ const alma = new Alma()
 const multer = require("multer");
 const path = require("path")
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 app.use('/public', express.static('public'));
 
 const storage = multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null,"uploads/");
+    destination: function (req, file, cb) {
+        cb(null, "uploads/");
     },
-    filename: function(req,file,cb){
+    filename: function (req, file, cb) {
         cb(null, file.originalname + Date.now() + path.extname(file.originalname));
     }
 })
 
-const upload = multer({storage});
+const upload = multer({
+    storage
+});
 
-app.get("/upload",(req,res) => {
+app.get("/upload", (req, res) => {
     res.render("upload", {
         title: 'Dual - Upload'
     });
 })
 
-app.post("/upload/",upload.single("file"),(req,res)=>{
+app.post("/upload/", upload.single("file"), (req, res) => {
     res.render("upload", {
         title: 'Dual - Upload'
     });
 })
 
-app.get('/', (req, res) => {    
+app.get('/', (req, res) => {
     res.redirect('home')
 });
 
-app.get('/pagina_inicial', (req, res) => {    
+app.get('/pagina_inicial', (req, res) => {
     res.render("pag_inicial", {
         title: 'Dual - PaginaInicial'
     })
@@ -55,7 +57,6 @@ app.get('/login', (req, res) => {
         title: 'Dual - Login'
     })
 })
-<<<<<<< HEAD
 app.get('/cadastro', (req, res) => {
     res.render('register', {
         title: 'Dual - Cadastro'
@@ -63,17 +64,18 @@ app.get('/cadastro', (req, res) => {
 })
 
 app.use((req, res, next) => {
-=======
-app.get('/atividades',(req, res) =>{
-    res.render("atividades", {title: 'Dual - Atividades'});
-})
-app.use(function (req, res, next){
->>>>>>> origin/requisito_rf0007
+    app.get('/atividades', (req, res) => {
+        res.render("atividades", {
+            title: 'Dual - Atividades'
+        });
+    })
+});
+
+app.use(function (req, res, next) {
     res.status(404).render("404", {
         title: '404 - Página não encontrada'
     });
 });
-
 
 
 module.exports = app;
